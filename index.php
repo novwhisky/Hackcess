f565c07d5ac33eca9a3d92f74f069015e48405d9
Nathan Witt <nathanwitt@jellyvision.com>
2012-03-28 16:56:29 -0500

    .gitignore maintenenace

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+error_reporting(E_ALL);
+if(!include_once 'hc-config.php') die("<h1>Missing hc-config.php</h1><p>Hackcess can't work without a file named hc-config.php that should be located in the root of this install.</p>");
